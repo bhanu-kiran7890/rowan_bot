d2a905ad17c4483f5613107174c979de50601047
Bhanu Kiran Guda <143862280+bhanu-kiran7890@users.noreply.github.com>
2025-12-03 20:21:59 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from chatbot import ask_rowan_bot
 app = FastAPI()
 
 # Mount static folder
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def home():
@@ -36,3 +36,4 @@ async def ask_bot(request: Request):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
