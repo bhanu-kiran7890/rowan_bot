7b49a69b285837d613f8a3a1955e249698f732c5
Bhanu Kiran Guda <143862280+bhanu-kiran7890@users.noreply.github.com>
2025-12-06 10:08:32 -0500

    Update ingest.py

--- app/ingest.py
+++ app/ingest.py
@@ -2,6 +2,7 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import OllamaEmbeddings  # type: ignore
 
 import os
 
